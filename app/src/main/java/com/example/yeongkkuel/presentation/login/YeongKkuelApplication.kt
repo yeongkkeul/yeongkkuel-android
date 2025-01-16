3eb39c47e6b6934b8a5b74dce2c05eb4dcc682b0
kimyuchan <rladbcks103@naver.com>
2025-01-16 20:55:34 +0900

    refactor/#6 : 디자인 시스템 적용, 키해시 삭제

--- app/src/main/java/com/example/yeongkkuel/presentation/login/YeongKkuelApplication.kt
+++ app/src/main/java/com/example/yeongkkuel/presentation/login/YeongKkuelApplication.kt
@@ -1,13 +1,11 @@
 package com.example.yeongkkuel.presentation.login
 
 import android.app.Application
-import android.util.Log
 import com.example.yeongkkuel.BuildConfig.kakao_NATIVE_APP_KEY
 import com.kakao.sdk.common.KakaoSdk
-import com.kakao.sdk.common.util.Utility
 
 // kakao sdk 연동 (초기화작업)
-class GlobalApplication : Application() {
+class YeongKkuelApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
@@ -15,8 +13,6 @@ class GlobalApplication : Application() {
 
         // Kakao SDK 초기화
         KakaoSdk.init(this, kakao_NATIVE_APP_KEY)
-        // 키 해시 확인
-        var keyHash = Utility.getKeyHash(this)
-        Log.d("GlobalApplication", "$keyHash")
+
     }
 }
