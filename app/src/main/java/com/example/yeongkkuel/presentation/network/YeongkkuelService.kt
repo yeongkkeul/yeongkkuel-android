d438da3149844a41ae52b97cb58d54dd0007fcbd
yang dong won <didehddnjs89@gmail.com>
2025-01-24 15:10:13 +0900

    feat/#3: retrofit Reponse general 생성

--- app/src/main/java/com/example/yeongkkuel/presentation/network/YeongkkuelService.kt
+++ app/src/main/java/com/example/yeongkkuel/presentation/network/YeongkkuelService.kt
@@ -1,4 +1,10 @@
 package com.example.yeongkkuel.presentation.network
 
+import com.example.yeongkkuel.presentation.network.response.Response
+import com.example.yeongkkuel.presentation.network.response.expenditure.DayExpenditureResponse
+import retrofit2.http.GET
+
 interface YeongkkuelService {
+    @GET("/api/expenditures/day")
+    fun getExpendituresDay(): Response<DayExpenditureResponse>
 }
