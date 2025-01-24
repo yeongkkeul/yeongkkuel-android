5480549a18fa5190624bf0f01cdae7129debe5ab
kimyuchan <rladbcks103@naver.com>
2025-01-24 16:45:10 +0900

    feature/#22: 하루 지출 목표액 설정

--- app/src/main/java/com/example/yeongkkuel/presentation/my/DailyExpenseGoalFragment.kt
+++ app/src/main/java/com/example/yeongkkuel/presentation/my/DailyExpenseGoalFragment.kt
@@ -1,60 +1,80 @@
 package com.example.yeongkkuel.presentation.my
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.navigation.fragment.findNavController
 import com.example.yeongkkuel.R
+import com.example.yeongkkuel.databinding.FragmentDailyExpenseGoalBinding
+import com.example.yeongkkuel.databinding.FragmentEditProfileBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DailyExpenseGoalFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DailyExpenseGoalFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentDailyExpenseGoalBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daily_expense_goal, container, false)
+    ): View {
+        _binding = FragmentDailyExpenseGoalBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DailyExpenseGoalFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DailyExpenseGoalFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // EditText에 포커스를 설정하고 키보드 표시
+        val editText = binding.etAmount
+        editText.requestFocus()
+
+        // 키보드 강제 표시
+        val inputMethodManager = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
+
+
+        // 뒤로가기
+        binding.ivBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.tvEdit.setOnClickListener() {
+            // TODO: 하루목표지출액 수정 - 백엔드 전송
+        }
+
+        binding.etAmount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val amount = s.toString().replace(",", "").toIntOrNull() ?: 0
+                val formatted = String.format("%,d", amount)
+                binding.etAmount.removeTextChangedListener(this)
+                binding.etAmount.setText(formatted)
+                binding.etAmount.setSelection(formatted.length)
+                binding.etAmount.addTextChangedListener(this)
             }
+        })
+
+
     }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
+
+
 }
