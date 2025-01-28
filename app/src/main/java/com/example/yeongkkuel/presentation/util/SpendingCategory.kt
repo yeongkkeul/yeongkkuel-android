74e49d0370a7828f1975664ea1ff69bfae214949
Jihyun <twentyt.park@gmail.com>
2025-01-28 16:43:00 +0900

    fix/#36: 빌드 오류 해결

--- app/src/main/java/com/example/yeongkkuel/presentation/util/SpendingCategory.kt
+++ app/src/main/java/com/example/yeongkkuel/presentation/util/SpendingCategory.kt
@@ -6,4 +6,17 @@ sealed class SpendingCategory(val kor: String) {
     object BEAUTY : SpendingCategory("화장품")
     object IMPROVEMENT : SpendingCategory("개선")
     data class CUSTOM(val name: String) : SpendingCategory(name)  // CUSTOM은 name을 동적으로 받는다.
+
+    companion object {
+        // fromKor 함수 추가
+        fun fromKor(kor: String): SpendingCategory {
+            return when (kor) {
+                SNACK.kor -> SNACK
+                SHOP.kor -> SHOP
+                BEAUTY.kor -> BEAUTY
+                IMPROVEMENT.kor -> IMPROVEMENT
+                else -> CUSTOM(kor) // 등록되지 않은 경우 CUSTOM으로 처리
+            }
+        }
+    }
 }
