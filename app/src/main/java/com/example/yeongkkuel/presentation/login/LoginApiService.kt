c72d2e4ec61a2ff8b35f12a1fc6d63fc3f0474e2
kimyuchan <rladbcks103@naver.com>
2025-02-01 17:56:43 +0900

    feat/#33: 회원기입 API 연결

--- app/src/main/java/com/example/yeongkkuel/presentation/login/LoginApiService.kt
+++ app/src/main/java/com/example/yeongkkuel/presentation/login/LoginApiService.kt
@@ -1,7 +1,9 @@
 package com.example.yeongkkuel.presentation.login
 
+import com.example.yeongkkuel.presentation.login.request.ReferralRequest
 import com.example.yeongkkuel.presentation.login.request.UserInfoRequest
 import com.example.yeongkkuel.presentation.login.response.KakaoLoginResponse
+import com.example.yeongkkuel.presentation.login.response.ReferralResponse
 import com.example.yeongkkuel.presentation.login.response.UserInfoResponse
 import retrofit2.Call
 import retrofit2.http.Body
@@ -14,12 +16,17 @@ interface LoginApiService {
 
     @GET("/api/auth/kakao-login/")
     fun kakaoLogin(
-        @Query("code") code: String
+        @Query("accessToken") Token: String
     ) : Call<KakaoLoginResponse>
 
     @POST("/api/auth/user-info")
     suspend fun postUserInfo(
         @Body request: UserInfoRequest
     ): UserInfoResponse
+
+    @POST("/api/api/recommend-code")
+    fun validateRecommendCode(
+        @Body request: ReferralRequest
+    ): ReferralResponse
 
 }
--- app/src/main/java/com/example/yeongkkuel/presentation/login/request/UserInfoRequest.kt
+++ app/src/main/java/com/example/yeongkkuel/presentation/login/request/UserInfoRequest.kt
@@ -2,7 +2,8 @@ package com.example.yeongkkuel.presentation.login.request
 
 data class UserInfoRequest(
     val nickName: String,
-    val gender: String,
-    val ageGroup: String,
+    val gender: String, //STUDENT,EMPLOYEE,HOMEMAKER,SELF_EMPLOYED,UNDECIDED
+    val ageGroup: String, //UNDECIDED,TEENAGER,TWENTIES,THIRTIES,FORTIES,FIFTIES,SIXTIES_AND_ABOVE
     val job: String
 )
+
