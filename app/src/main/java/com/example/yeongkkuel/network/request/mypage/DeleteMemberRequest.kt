5fd3614fc560b48d86d1adabe990879a932bc180
yang dong won <didehddnjs89@gmail.com>
2025-02-08 10:35:34 +0900

    feat/#57: myPage service interface

---
+++ app/src/main/java/com/example/yeongkkuel/network/request/mypage/DeleteMemberRequest.kt
@@ -0,0 +1,8 @@
+package com.example.yeongkkuel.network.request.mypage
+
+import com.google.gson.annotations.SerializedName
+
+data class DeleteMemberRequest(
+    @SerializedName("reason") val reason: String,
+    @SerializedName("detail") val detail: String
+)
--- app/src/main/java/com/example/yeongkkuel/network/service/MyPageService.kt
+++ app/src/main/java/com/example/yeongkkuel/network/service/MyPageService.kt
@@ -1,10 +1,13 @@
 package com.example.yeongkkuel.network.service
 
+import com.example.yeongkkuel.network.request.mypage.DeleteMemberRequest
 import com.example.yeongkkuel.network.request.mypage.PatchMyPageRequest
 import com.example.yeongkkuel.network.response.Response
-import com.example.yeongkkuel.network.response.mypage.MyPagePatchResult
 import com.example.yeongkkuel.network.response.mypage.MyPageResult
+import com.example.yeongkkuel.network.response.mypage.RewardsResult
+import com.example.yeongkkuel.network.response.mypage.UserReferralCodeResult
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.PATCH
 
@@ -17,5 +20,19 @@ interface MyPageService {
     @PATCH("/api/mypage")
     suspend fun patchMyPage(
         @Body request: PatchMyPageRequest
-    ): Response<MyPagePatchResult>
+    ): Response<MyPageResult>
+
+    // 추천인 코드 조회
+    @GET("/api/userreferralcode")
+    suspend fun getUserReferralCode(): Response<UserReferralCodeResult>
+
+    // 리워드 목록 조회
+    @GET("/api/rewards")
+    suspend fun getRewards(): Response<RewardsResult>
+
+    // 회원탈퇴
+    @DELETE("/api/auth/delete")
+    suspend fun deleteMember(
+        @Body request: DeleteMemberRequest
+    ):Response<String>
 }
