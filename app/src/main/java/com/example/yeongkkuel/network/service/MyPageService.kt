8154421bcf4bb1ee0f2b66dadc548c9eb037d45a
kimyuchan <rladbcks103@naver.com>
2025-02-09 20:29:18 +0900

    refactor/#48: 프로필 수정 dto 수정

--- app/src/main/java/com/example/yeongkkuel/network/service/MyPageService.kt
+++ app/src/main/java/com/example/yeongkkuel/network/service/MyPageService.kt
@@ -6,13 +6,16 @@ import com.example.yeongkkuel.network.response.Response
 import com.example.yeongkkuel.network.response.mypage.MyPageResult
 import com.example.yeongkkuel.network.response.mypage.RewardsResult
 import com.example.yeongkkuel.network.response.mypage.UserReferralCodeResult
+import okhttp3.MultipartBody
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.PATCH
+import retrofit2.http.Part
 
 interface MyPageService {
-    //하루 목표 지출액 설정은 statService에서 사용되어 프론트에서 지출설정 기능까지 구현되어 있습니다.
+    //하루 목표 지출액 설정은 statService에서 사용정되어 프론트에서 지출설 기능까지 구현되어 있습니다.
     // swagger에는 mypage 범주로 되어있기에 확인하면서 혼동되지 않도록 주석을 답니다. 읽었으면 지워주세요.
 
     // 마이페이지 프로필 조회
@@ -20,9 +23,11 @@ interface MyPageService {
     suspend fun getMyPage(): Response<MyPageResult>
 
     // 마이페이지 프로필 수정
+    @Multipart
     @PATCH("/api/mypage")
     suspend fun patchMyPage(
-        @Body request: PatchMyPageRequest
+        @Part("myPageInfoRequestDto") request: PatchMyPageRequest,
+        @Part profileImage: MultipartBody.Part?
     ): Response<MyPageResult>
 
     // 추천인 코드 조회
