f2b91ae64df602f3b70350cd99bb7c33a9f261c7
calla20031 <calla20031@naver.com>
2025-02-16 16:30:30 +0900

    Modify API of ExpenseEdit(#76)

--- app/src/main/java/com/example/yeongkkuel/presentation/home/entry/data/ExpenseApiService.kt
+++ app/src/main/java/com/example/yeongkkuel/presentation/home/entry/data/ExpenseApiService.kt
@@ -3,9 +3,9 @@ package com.example.yeongkkuel.presentation.home.entry.data
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
-import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.DELETE
+import retrofit2.http.PATCH
 
 interface ExpenseApiService {
 
@@ -14,7 +14,7 @@ interface ExpenseApiService {
         @Body expenseRequest: ExpenseRequest
     ): Response<ExpenseResponse>
 
-    @PUT("/api/expense/{expenseId}")
+    @PATCH("/api/expense/{expenseId}")
     suspend fun updateExpense(
         @Path("expenseId") expenseId: Int,
         @Body request: ExpenseUpdateRequest
