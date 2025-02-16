124443a58c57b03d794b673902299e0f1f348fef
calla20031 <calla20031@naver.com>
2025-02-16 21:54:23 +0900

    Modify Reward_Dialog Yesterday at HomeFragment (#81)

--- app/src/main/java/com/example/yeongkkuel/presentation/home/HomeApiService.kt
+++ app/src/main/java/com/example/yeongkkuel/presentation/home/HomeApiService.kt
@@ -6,4 +6,7 @@ import retrofit2.http.GET
 interface HomeApiService {
     @GET("api/home") // ✅ 홈 화면 데이터 조회 API
     suspend fun getHomeData(): Response<HomeResponse>
+
+    @GET("/api/reward/yesterday")
+    suspend fun getYesterdayReward(): Response<RewardResponse>
 }
